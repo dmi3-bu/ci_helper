d48f53f8f3d01ccfa5875d56206d89e56f348bf7
dmitrii.buk <dmitrii.buk@cadolabs.io>
2025-10-03 19:14:18 +0700

    heavy specs distribution for RunSpecs

--- lib/ci_helper/commands/run_specs.rb
+++ lib/ci_helper/commands/run_specs.rb
@@ -21,12 +21,37 @@ module CIHelper
       end
 
       def job_files
+        p("PATCHED!!!!!!")
         all_files = path.glob("spec/**/*_spec.rb")
+        all_files_count = all_files.count
+        heavy_files = []
+        std_files   = []
+
+        all_files.each do |file|
+          relative_path = file.relative_path_from(path).to_s
+          if heavy_specs_paths.any? { |pattern| File.fnmatch?(pattern, relative_path) }
+            heavy_files << file
+          else
+            std_files << file
+          end
+        end
+
         sorted_files =
-          all_files.map { |x| [x.size, x.relative_path_from(path).to_s] }.sort.map(&:last)
-        sorted_files.reverse.select.with_index do |_file, index|
+          std_files.map { |x| [x.size, x.relative_path_from(path).to_s] }.sort.map(&:last)
+        (sorted_files + heavy_files).reverse.select.with_index do |_file, index|
           (index % job_count) == (job_index - 1)
         end
+      end
+
+      def heavy_specs_paths
+        %w[
+          spec/requests/admin/customers/orders_spec.rb
+          spec/requests/admin/customers/orders/download_spec.rb
+          spec/requests/admin/orders/download_spec.rb
+          spec/requests/admin/orders/index_spec.rb
+          spec/requests/admin/hung_withdraws/index_spec.rb
+          spec/requests/admin/reports/*
+        ]
       end
 
       def job_index
